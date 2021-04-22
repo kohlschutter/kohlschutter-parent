fae237bec519e5d8d3799f73a73b425f96381297
Christian Kohlschütter <christian@kohlschutter.com>
2021-04-21 22:36:52 -0700

    Add support for multirelease-jars, compiletime annotations
    This change adds a new "kohlschutter-parent-multirelease" parent POM
    that configures projects to emit jars that are compatible with a range
    of Java versions, specifically with Java 8 (pre-JPMS/Jigsaw) and Java 9
    or greater.

---
+++ compiler-annotations/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * When adding this dependency to your module-info, please use "require static" instead of
+ * "require".
+ */
+module com.kohlschutter.annotations.compiletime {
+  exports com.kohlschutter.annotations.compiletime;
+}

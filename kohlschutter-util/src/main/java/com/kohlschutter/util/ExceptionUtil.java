9235cd9ffa0d7d0bcc7fd4b3c3fe1b5da7cd4354
Christian Kohlschütter <christian@kohlschutter.com>
2021-05-08 18:52:09 -0700

    Add ExceptionUtil

---
+++ kohlschutter-util/src/main/java/com/kohlschutter/util/ExceptionUtil.java
@@ -0,0 +1,32 @@
+package com.kohlschutter.util;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Helper methods for exception handling.
+ * 
+ * @author Christian Kohlschütter
+ */
+public final class ExceptionUtil {
+  private ExceptionUtil() {
+    throw new IllegalStateException("No instances");
+  }
+
+  /**
+   * Unwraps the exception thrown as the cause of an {@link ExecutionException}, if possible.
+   * 
+   * If the cause is not an exception (but some other throwable), the {@link ExecutionException}
+   * itself is returned.
+   * 
+   * @param ex The {@link ExecutionException}.
+   * @return The {@link ExecutionException}, or its cause {@link Exception}.
+   */
+  public static Exception unwrapExecutionException(ExecutionException ex) {
+    Throwable cause = ex.getCause();
+    if (cause instanceof Exception) {
+      return ((Exception) cause);
+    } else {
+      return ex;
+    }
+  }
+}

8d884dd62dc7b5fec49a43fcb66074998f09269c
Christian Kohlschütter <christian@kohlschutter.com>
2021-07-20 21:34:45 -0700

    Add ConsolePrintStream

---
+++ kohlschutter-util/src/main/java/com/kohlschutter/util/ConsolePrintStream.java
@@ -0,0 +1,207 @@
+package com.kohlschutter.util;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * A {@code System.out} wrapper that can knows a little bit about the system console.
+ * 
+ * @author Christian Kohlschütter
+ */
+public class ConsolePrintStream extends PrintStream {
+  private static final byte[] NEWLINE_BYTES = System.lineSeparator().getBytes();
+  private static final int NEWLINE = '\n';
+
+  private final PrintStream out;
+  private final ConsoleFilterOut cfo;
+  private boolean closed = false;
+
+  private ConsolePrintStream() {
+    this(new ConsoleFilterOut(System.out));
+  }
+
+  public static ConsolePrintStream wrapSystemOut() {
+    synchronized (System.class) {
+      PrintStream out = System.out;
+      if (out instanceof ConsolePrintStream) {
+        return (ConsolePrintStream) out;
+      } else {
+        return new ConsolePrintStream();
+      }
+    }
+  }
+
+  private ConsolePrintStream(ConsoleFilterOut cfo) {
+    super(cfo);
+    this.cfo = cfo;
+    this.out = System.out;
+    System.setOut(this);
+  }
+
+  private static final class ConsoleFilterOut extends FilterOutputStream {
+    private int numBytes = 0;
+    private int lastNewline = 0;
+    private int markedPosition = 0;
+    private int lastUpdate = 0;
+    private int lastByte = 0;
+
+    private ConsoleFilterOut(PrintStream out) {
+      super(out);
+    }
+
+    @Override
+    public synchronized void write(int b) throws IOException {
+      boolean newline = (b == NEWLINE);
+      if (!newline && lastUpdate == numBytes && numBytes > 0) {
+        lastUpdate = 0;
+        write(NEWLINE_BYTES);
+      }
+
+      super.write(b);
+      lastByte = b;
+
+      int count = ++numBytes;
+      if (newline) {
+        lastNewline = count;
+      }
+    }
+
+    @Override
+    public synchronized void write(byte[] b, int off, int len) throws IOException {
+      if (len == 0) {
+        return;
+      }
+      if (b[off] != NEWLINE && lastUpdate == numBytes && numBytes > 0) {
+        lastUpdate = 0;
+        write(NEWLINE_BYTES);
+      }
+
+      super.write(b, off, len);
+      lastByte = b[off + len - 1];
+      int countPre = numBytes;
+      numBytes += len;
+
+      for (int i = len - 1; i >= off; i--) {
+        if (b[i] == NEWLINE) {
+          lastNewline = (countPre + (i - off + 1));
+        }
+      }
+    }
+
+    private synchronized void markPosition() {
+      markedPosition = numBytes;
+    }
+
+    private synchronized boolean hasNewlineSinceMark() {
+      return lastNewline > markedPosition;
+    }
+
+    private synchronized boolean hasOutputSinceMark() {
+      return numBytes > markedPosition;
+    }
+  }
+
+  /**
+   * Marks the current position in the stream (which by default is the beginning of the stream).
+   *
+   * @see #hasOutputSinceMark()
+   * @see #hasNewlineSinceMark()
+   * @see #update(String)
+   */
+  public void markPosition() {
+    cfo.markPosition();
+  }
+
+  /**
+   * Checks if a newline character was written since the last call to {@link #markPosition()}.
+   * 
+   * @return {@code true} if a newline character was found.
+   */
+  public boolean hasNewlineSinceMark() {
+    return cfo.hasNewlineSinceMark();
+  }
+
+  /**
+   * Checks if any output was written since the last call to {@link #markPosition()}.
+   * 
+   * @return {@code true} if a newline character was found.
+   */
+  public boolean hasOutputSinceMark() {
+    return cfo.hasOutputSinceMark();
+  }
+
+  /**
+   * Prints a string, not advancing to a newline unless necessary, and clearing any previous output
+   * written with {@link #update(String)} via backspace characters.
+   * 
+   * This allows for succinct progress updates that don't pollute the console output.
+   * 
+   * @param s The string to print.
+   */
+  public void update(String s) {
+    synchronized (cfo) {
+      cfo.lastUpdate = 0;
+      if (cfo.lastByte != NEWLINE) {
+        if (hasNewlineSinceMark()) {
+          println();
+        } else {
+          clearLine();
+        }
+      }
+      print(s);
+      flush();
+      markPosition();
+      cfo.lastUpdate = cfo.numBytes;
+    }
+  }
+
+  /**
+   * Like {@link #update(String)}, but adding a newline after the output.
+   * 
+   * @param s The string to print.
+   */
+  public void updateln(String s) {
+    synchronized (cfo) {
+      update(s);
+      println();
+    }
+  }
+
+  private void clearLine() {
+    flush();
+
+    int numBytesSinceNewline = (cfo.numBytes - cfo.lastNewline);
+    if (numBytesSinceNewline == 0) {
+      return;
+    }
+    StringBuilder sb = new StringBuilder(numBytesSinceNewline * 3 + 1);
+
+    // Reliably overwrite anything written so far in the last line without ANSI escape sequences
+    for (int i = 0; i < numBytesSinceNewline; i++) {
+      sb.append("\b \b");
+    }
+
+    // triggers a newline in Eclipse when "Interpret ASCII control characters" is off
+    sb.append("\r");
+
+    out.print(sb);
+    out.flush();
+    cfo.lastNewline = cfo.numBytes;
+  }
+
+  /**
+   * Flushes all pending data to the previous System.out, restoring (but not closing) it.
+   */
+  @Override
+  public void close() {
+    synchronized (cfo) {
+      flush();
+      if (closed) {
+        return;
+      }
+      closed = true;
+      System.setOut(out);
+    }
+  }
+}
---
+++ kohlschutter-util/src/test/java/com/kohlschutter/util/ConsolePrintStreamTest.java
@@ -0,0 +1,63 @@
+package com.kohlschutter.util;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+public class ConsolePrintStreamTest {
+
+  @Test
+  public void testNewlineMarker() throws Exception {
+    synchronized (System.class) {
+      PrintStream outOld = System.out;
+      try {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        try (ConsolePrintStream cpw = ConsolePrintStream.wrapSystemOut()) {
+          assertFalse(cpw.hasNewlineSinceMark());
+          cpw.println();
+          assertTrue(cpw.hasNewlineSinceMark());
+          cpw.markPosition();
+          assertFalse(cpw.hasNewlineSinceMark());
+          cpw.print('1');
+          assertFalse(cpw.hasNewlineSinceMark());
+          cpw.print('\r');
+          assertFalse(cpw.hasNewlineSinceMark());
+          cpw.println("Hello World");
+          assertTrue(cpw.hasNewlineSinceMark());
+          cpw.markPosition();
+          assertFalse(cpw.hasNewlineSinceMark());
+          cpw.print(new char[] {'\n'});
+          assertTrue(cpw.hasNewlineSinceMark());
+        }
+      } finally {
+        System.setOut(outOld);
+      }
+    }
+  }
+
+  @Test
+  public void testClearLine() throws Exception {
+    try (ConsolePrintStream cpw = ConsolePrintStream.wrapSystemOut()) {
+      cpw.updateln("Hello");
+      cpw.update("World");
+      cpw.updateln("Java");
+      for (int i = 0; i < 5; i++) {
+        String s = UUID.randomUUID().toString();
+        cpw.update(s.substring(0, s.length() - i));
+
+        if (i == 3) {
+          System.out.println("Some interrupting text");
+        }
+
+        Thread.sleep(100);
+      }
+      cpw.println();
+    }
+  }
+}

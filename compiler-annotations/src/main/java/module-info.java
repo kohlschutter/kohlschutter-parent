fa5e2e65efd432a2a945b62ccf8cbf016ca2ea38
Christian Kohlschütter <christian@kohlschutter.com>
2023-06-04 13:36:27 +0200

    Clarify use of compiler-annotations dependency

--- compiler-annotations/src/main/java/module-info.java
+++ compiler-annotations/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 /**
- * When adding this dependency to your module-info, please use "require static" instead of
- * "require".
+ * When adding this dependency to your module-info, please use:
+ * <p>
+ * <code> requires static transitive
+ * com.kohlschutter.annotations.compiletime; <br/>
+ * requires static transitive org.eclipse.jdt.annotation;
+ * </code>
  */
 module com.kohlschutter.annotations.compiletime {
   exports com.kohlschutter.annotations.compiletime;
+
+  requires transitive org.eclipse.jdt.annotation;
 }

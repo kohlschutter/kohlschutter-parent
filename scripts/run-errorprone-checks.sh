c86e83146c05d878d0b42094d740c3015b96b8f5
Christian Kohlschütter <christian@kohlschutter.com>
2023-09-24 19:57:04 +0200

    Add errorprone check script

---
+++ scripts/run-errorprone-checks.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+#
+# Runs "google errorprone" checks on this Maven project
+# 
+# Copyright 2023 by Christian Kohlschütter
+# SPDX-License-Identifier: Apache-2.0
+#
+
+cd "$(dirname $0)"/..
+
+opts=()
+if [[ $#@ -eq 0 ]]; then
+	opts=( )
+else
+	opts=( $@ )
+fi
+
+out=()
+
+while read l; do
+	#echo $l;
+	case "$l" in
+		"[INFO]"*)
+			;;
+		"[WARNING] To suppress warnings about obsolete options"*)
+			;;
+		"[WARNING]"*)
+			echo
+			echo $l;
+			;;
+		"--")
+			;;
+		*)
+			echo $l
+			;;
+	esac
+done < <(
+MAVEN_OPTS="--add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" mvn clean compile ${opts[@]} -Perrorprone | grep -C2 errorprone.info )

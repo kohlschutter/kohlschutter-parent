f796992f3b6d6421422b30e60e9f6c59d561b993
Christian Kohlschütter <christian@kohlschutter.com>
2024-12-12 21:57:41 +0100

    util, test-util: Fix org.eclipse.jdt.annotation in module-info

--- kohlschutter-test-util/src/main/java/module-info.java
+++ kohlschutter-test-util/src/main/java/module-info.java
@@ -5,8 +5,8 @@ module com.kohlschutter.testutil {
   exports com.kohlschutter.testutil;
 
   requires static com.kohlschutter.annotations.compiletime;
+  requires static org.eclipse.jdt.annotation;
   requires transitive org.junit.jupiter.api;
   requires transitive com.kohlschutter.util;
   requires java.logging;
-  requires org.eclipse.jdt.annotation;
 }
--- kohlschutter-util/src/main/java/module-info.java
+++ kohlschutter-util/src/main/java/module-info.java
@@ -3,9 +3,8 @@
  */
 module com.kohlschutter.util {
   requires static com.kohlschutter.annotations.compiletime;
+  requires static org.eclipse.jdt.annotation;
+  requires static java.management;
 
   exports com.kohlschutter.util;
-
-  requires org.eclipse.jdt.annotation;
-  requires static java.management;
 }
